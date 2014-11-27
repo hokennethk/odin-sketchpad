8c91e005aaafc7a850773155d7c33c9e087f5bbe
Kenneth Ho <hokennethk@users.noreply.github.com>
2014-11-26 17:03:41 -0800

    added comments

--- application.js
+++ application.js
@@ -1,4 +1,4 @@
-// initial vars
+// initial gridSize
 var gridSize = 16;
 
 // Functions
@@ -16,23 +16,28 @@ function newGrid() {
 }
 
 function createGrid(gridSize) {
-	// define tbody outside of for-loop for performance
+	// define tbody outside of for-loop for performance (don't know if this actually improved performance though...)
 	var tbody = $("#grid").find("tbody");
 
-
-	// create initial 16x16 grid of divs
+	// add table rows
 	for (i=0; i<gridSize; i++) {
 	    tbody.append("<tr class='row'></tr>");
 	}
-	// define outside of for-loop for performance
+
+	// define outside of for-loop for performance (don't know if this actually improved performance...)
 	var trow = $("tr.row");
 
+	// add table cells
 	for (i=0; i<gridSize; i++) {
 	    trow.append("<td><div></div></td>");
 	}
 
 	/*
 	Current table grid size is based off of height. Set width to be equal to height size for "squareness"
+	This is assuming browser size has a smaller height than width. Creates table width based off of max height size so
+	that the whole grid/table fits in the current browser without having to scroll up/down
+
+	Not required, but nice to have
 	*/
 	var tableHeight = $('table#grid').height();
 	$('table#grid').width(tableHeight);
@@ -46,15 +51,10 @@ function createGrid(gridSize) {
 }
 
 
-
-
-
 $(document).ready(function() {	
 	// create initial grid
 	createGrid(gridSize);
 });
-// Main logic
-
 
 
 
